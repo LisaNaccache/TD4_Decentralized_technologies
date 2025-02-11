98107410b707b3aa2ab63b6951229c3d709e61ab
LisaNaccache <naccachelisa@gmail.com>
2025-02-11 15:06:49 +0100

    2.1 Nodes' GET routes

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -7,8 +7,27 @@ export async function simpleOnionRouter(nodeId: number) {
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
   // TODO implement the status route
   onionRouter.get("/status", (req, res) => { res.send("live");});
+
+  // Get the last encrypted message received
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  // Get the last decrypted message received
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  // Get the destination of the last message received
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
