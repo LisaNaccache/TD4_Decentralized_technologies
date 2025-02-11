12ea8a1831c9aeb25046b3ee829f8ca1a07c6238
LisaNaccache <naccachelisa@gmail.com>
2025-02-11 15:09:52 +0100

    2.2 Users' GET routes

--- src/users/user.ts
+++ src/users/user.ts
@@ -7,8 +7,21 @@ export async function user(userId: number) {
   _user.use(express.json());
   _user.use(bodyParser.json());
 
+  let lastReceivedMessage: string | null = null;
+  let lastSentMessage: string | null = null;
+
   // TODO implement the status route
   _user.get("/status", (req, res) => {res.send("live");});
+
+  // Get the last encrypted message received
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  // Get the last message sent
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
+  });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
